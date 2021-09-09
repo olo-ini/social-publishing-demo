15dc24e0c7b7472e442dd28d0841961d5c2dd752
Ini <ini@Inis-Air.hitronhub.home>
2021-09-09 10:36:18 -0500

    all files

---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    MONGODB: 'mongodb+srv://new-user-01:0nVGJwGw4lPZWAug@cluster0.zvmai.mongodb.net/Leartifydb?retryWrites=true&w=majority',
+    SECRET_KEY: 'some secret key'
+}
